17bd8e37022cb0c9ad227090c02fdfd4b425063b
Richard Afro <richyafro@hotmail.com>
2018-04-26 14:47:28 +0100

    Analyze message using sentiment analysis

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const Pusher = require('pusher');
+const Sentiment = require('sentiment');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -12,6 +13,8 @@ const pusher = new Pusher({
   cluster: 'eu',
   encrypted: true,
 });
+
+const sentiment = new Sentiment();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,11 +30,19 @@ app.use((req, res, next) => {
 app.post('/messages', (req, res) => {
   const { body } = req;
   const { text, id } = body;
+  const result = sentiment.analyze(text);
+  const comparative = result.comparative;
+  const tone =
+    comparative >= 0 ? (comparative >= 1 ? 'positive' : 'neutral') : 'negative';
 
   const data = {
     text,
     id,
     timeStamp: new Date(),
+    sentiment: {
+      tone,
+      score: result.score,
+    },
   };
 
   pusher.trigger('chat', 'message', data);
